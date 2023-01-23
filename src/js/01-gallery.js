56b7832e436a962560baf7c52414273bd68aff51
OleksiiKostenko <kostenko.oleksii91@gmail.com>
2023-01-23 23:45:54 +0200

    video work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,3 @@
-console.log('Hello');
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,33 +2,20 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 
-
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 const CURRENT_TIME_VIDEO = "videoplayer-current-time";
 
+
 player.on('timeupdate', throttle(onPlay, 1000));
 
-    player.on('play', function() {
-        console.log('played the video!');
-    });
 
-const onPlay = function (evt) {
+ function onPlay(evt) {
     localStorage.setItem(CURRENT_TIME_VIDEO, evt.seconds);
 };
-const currentTime = localStorage.getItem(CURRENT_TIME_VIDEO)
-player.setCurrentTime(currentTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
 
-        default:
-            // some other error occurred
-            break;
-    }
-});
+const currentTime = localStorage.getItem(CURRENT_TIME_VIDEO);
+
+player.setCurrentTime(currentTime);
 
